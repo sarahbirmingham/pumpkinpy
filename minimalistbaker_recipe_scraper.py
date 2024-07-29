c64cd8e247d7cb360f4d58868348073ae1daa0e1
Sarah B <sarah.r.birmingham@gmail.com>
2024-07-29 00:41:15 -0700

    Update minimalistbaker_recipe_scraper.py
    Refactored:
    - no longer writes to .csv but instead passes dataframe back to main class
    - made fixes to recipe parser
    - better text output to track progress
    - added a timer
    - removed condensed recipe dataframe function; only option is to start with exploded ingredient lists

--- minimalistbaker_recipe_scraper.py
+++ minimalistbaker_recipe_scraper.py
@@ -4,10 +4,20 @@
 import pandas as pd
 import numpy as np
 import re
+import time
 import pumpkinpy as ppy
 from urllib.request import urlopen
 
 # Iterate through MinimalistBaker recipe index pages and scrape indiviual recipe URLs
+
+def pass_time(start):
+
+    end = time.time()
+    elapsed = (end - start)
+    minutes = int(np.floor(elapsed/60))
+    seconds = int(elapsed % 60)
+
+    print('\nTime elapsed: %d minutes and %d seconds' % (minutes, seconds))
 
 def get_recipe_urls(page):
 
@@ -32,122 +42,126 @@ def get_recipe_urls(page):
     
     return(new_urls)
 
-# Parses ingredient list by separating each unformatted ingredient into amount (1), unit (Tbsp), item (sugar), notes (or sub maple syrup)
+def get_ingredient_notes(ingredient_string):
 
-def ingredient_parser(ingredient_list):
+    # Notes are almost always included in parentheses at the end of a string
+    p1 = ingredient_string.rfind('(')
+    p2 = ingredient_string.rfind(')')
+     
+    # Separate out text contained in parentheses and remove any found notes from remaining ingredient string
+    if p1 != -1:
+        notes = ingredient_string[p1:p2+1]
+        ingredient_string = ingredient_string.replace(notes, '').strip()
+        notes = re.sub('[()]', '', notes)
+        notes = notes.strip()
+    else:
+        notes = ''
 
+    return ((ingredient_string, notes))
+
+def get_ingredient_amount(ingredient_string):
+
+    # # # Search for amount
+
+    # If we just extract all numbers, the following case becomes an issue:
+        # 4 salmon fillets, 4 oz each -> 44
+
+    # If we just extract numbers before the first space, the following case becomes an issue:
+        # 1 1/2 cups of flour -> 1
+
+    # So the 'amount' is usually a number, eventually followed by letters (e.g. 1 cup, 2 tbsp, 3 whole, 3/4 tsp)
+        # 4 salmon fillets, 4 oz each -> 4 salmon
+        # 1 1/2 cups of flour         -> 1 1/2 cups
+    
+
+    amt_key = "[^0-9] [a-zA-Z]+ "
+
+    amt = re.sub(amt_key, '', ingredient_string)
+    amt = re.sub("/$|-$", '', amt)
+
+    first_text = re.search("[a-zA-Z]", amt)
+    
+    try:
+        amt = amt[:first_text.start()].strip()
+
+    except:
+        amt = amt.strip()
+
+    # Remove 'amount' from remaining ingredient string
+    ingredient_string = ingredient_string.replace(amt, '').strip()
+
+    return((ingredient_string, amt))
+
+def get_ingredient_unit(ingredient_string, amt):
+     
+     # If we find a non-empty amount, the unit is typically the proceeding word
+
+     if amt == '':
+          return ((ingredient_string, amt, ''))
+     
+     unit = ingredient_string[:ingredient_string.find(' ')].strip()
+     item = ingredient_string.replace(unit, '').strip()
+
+     return(item, amt, unit)
+
+def ingredient_parser(ingredient_text):
+     
+     # Parses ingredient list by separating each unformatted ingredient into amount (1), unit (Tbsp), item (sugar), notes (or sub maple syrup)
+     
     new_ingredient_list = []
 
-    for item in ingredient_list:
+    for ingredient_string in ingredient_text:
 
-        # quick clean-up
-        item = item.replace('¼', '1/4').replace("⅓", '1/3').replace('½', '1/2').replace('⅔', '2/3').replace('¾', '3/4')
-        item = item.replace("\"", '').replace('*', '').replace(' to ', '-').replace('//', '/').replace('((', '(').replace('))', ')')
+        # Quick clean-up
+        ingredient_string = ingredient_string.replace('¼', '1/4').replace("⅓", '1/3').replace('½', '1/2').replace('⅔', '2/3').replace('¾', '3/4')
+        ingredient_string = ingredient_string.replace("\"", '').replace('*', '').replace(' to ', '-').replace('//', '/').replace('((', '(').replace('))', ')')   
         
-
-        # # # Find notes first
-        # most 'notes' on MinimalistBaker are enclosed within parentheses, making them easy to find
-        if item.find('(') != -1:
-             notes = item[item.rfind('('):item.rfind(')')+1]
-        else:
-             notes = ''
-
-        # If notes are found, remove them from remaining ingredient string             
-        if notes != '':
-             item = item.replace(notes, '')
-
-        notes = notes.replace('(', '').replace(')', '')
-
-        # # # Search for amount
-        # The 'amount' is usually a number, eventually followed by letters (e.g. 1 cup, 2 tbsp, 3 whole, 3/4 tsp)
-        key = "[^0-9] [a-zA-Z]+ "
-
-        amount = re.sub(key, '', item)
-
-        try:
-            if amount[-1] == '/':
-                    amount = amount.replace('/', '').strip()
-                        
-            if amount[-1] == '-':
-                    amount = amount.replace('-', '').strip()
-        
-        except:
-            pass
-
-        # Remove letters from amount, keep other quantities (e.g. 1-2, 1/2)
-        first_text = re.search("[a-zA-Z]", amount)
-    
-        try:
-            amount = amount[:first_text.start()]
-
-        except:
-             amount = amount
-
-        # Remove 'amount' from remaining ingredient string
-        item = item.replace(amount, '').strip()
-
-        # # # Find the unit
-        # The unit is typically the first word after the amount
-        if amount != '':
-            unit = item[:item.find(' ')]
-        else:
-            unit = ''
-
-        # # # Find the item
-        # The item is what's left over
-        item = item.replace(unit, '').strip()
+        (ingredient_string, notes) = get_ingredient_notes(ingredient_string)
+        (ingredient_string, amount) = get_ingredient_amount(ingredient_string)
+        (item, amount, unit) = get_ingredient_unit(ingredient_string, amount)
 
         new_ingredient_list.append(ppy.Ingredient(item, unit, amount, notes))
+        
+    return (new_ingredient_list)
 
-    return(new_ingredient_list)
+def get_recipe_nutrition_info(text):
 
-# Accesses each URL and scrapes nutrition info, ingredient list, and tags
-
-def get_recipe_info(url):
-    
-    page = urlopen(url)
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-
-    # # Title
-
-    # For MinimalistBaker, the title is always included in the URL
-    title = url.replace('https://minimalistbaker.com/', '').replace('/', '').replace('-', ' ')
-    title = title.title()
-
-    # # Nutrition information
-
-    # Nutrition information is provided and labeled on the MB website
     try:
         nutrition_key = "wprm-nutrition-label-text-nutrition-label.*</span>"
-        nutrition_text = re.findall(nutrition_key, html)
-        nutrition_text = nutrition_text[0]
+        nutrition_content = re.findall(nutrition_key, text)
+        nutrition_content = nutrition_content[0]
 
         find_labels = ">[A-Z][a-z]* *[A-Za-z]*: <"
         find_values = ">[0-9.]+<"
-        labels = re.findall(find_labels, nutrition_text)
-        values = re.findall(find_values, nutrition_text)
+        labels = re.findall(find_labels, nutrition_content)
+        values = re.findall(find_values, nutrition_content)
 
         labels = [l.replace('>', '').replace(': <', '') for l in labels]
         values = [v.replace('>', '').replace('<', '') for v in values]
         values = [float(v) for v in values]
 
-        nutrition = {}
+        nutrition_info = {}
 
         for i in range(len(labels)):
-            nutrition[labels[i]] = values[i]
+            nutrition_info[labels[i]] = values[i]
 
-        nutrition = ppy.Nutrition(calories = nutrition['Calories'], protein = nutrition['Protein'], fat = nutrition['Fat'], carbs = nutrition['Carbohydrates'])
+        nutrition = ppy.Nutrition(calories = nutrition_info['Calories'], 
+                                  protein = nutrition_info['Protein'], 
+                                  fat = nutrition_info['Fat'], 
+                                  carbs = nutrition_info['Carbohydrates'])
     
     except:
         nutrition = ppy.Nutrition(calories = 0, protein = 0, fat = 0, carbs = 0)
 
-    # # # Ingredients 
+    return (nutrition)
+
+def get_recipe_ingredients(text):
     
     # Ingredients are also labeled
+
     try:
         ingredient_key = "recipeIngredient\":.*recipeInstructions"
-        ingredient_text = re.findall(ingredient_key, html)
+        ingredient_text = re.findall(ingredient_key, text)
         ingredient_text = ingredient_text[0]
 
         ingredients = ingredient_text[ingredient_text.index("["):ingredient_text.index("]")]
@@ -161,131 +175,122 @@ def get_recipe_info(url):
 
     ingredient_list = ingredient_parser(ingredients)
 
+    return (ingredient_list)
 
-    # # # Classification
+def get_recipe_tags(text):
 
-    # Categories are labeled
     try: 
-        cat_key = "articleSection.*],\"inLanguage\""
-        cat_text = re.findall(cat_key, html)
-        cat_text = cat_text[0]
+        tag_key = "articleSection.*],\"inLanguage\""
+        tag_text = re.findall(tag_key, text)
+        tag_text = tag_text[0]
 
-        categories = cat_text[cat_text.find('[')+1:cat_text.find(']')]
-        categories = categories.replace("\"", '').replace(",", ", ")
+        recipe_tags = tag_text[tag_text.find('[')+1:tag_text.find(']')]
+        recipe_tags = recipe_tags.replace("\"", '').replace(",", ", ")
 
-        categories = categories.split(', ')
+        recipe_tags = recipe_tags.split(', ')
     
     except:
-        categories = []
+        recipe_tags = []
 
+    return (recipe_tags)
+
+def get_recipe_info(url):
+
+    # Accesses each URL and scrapes nutrition info, ingredient list, and tags
+    
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+
+    # For MinimalistBaker, the recipe title is always included in the URL
     title = url.replace('https://minimalistbaker.com/', '').replace('/', '').replace('-', ' ')
     title = title.title()
 
-    recipe = ppy.Recipe(title = title, ingredients = ingredient_list, nutrition = nutrition, tags = categories, url = url)
+    ingredient_list = get_recipe_ingredients(html)
+    nutrition_info = get_recipe_nutrition_info(html)
+    recipe_tags = get_recipe_tags(html)
+
+    recipe = ppy.Recipe(title = title, ingredients = ingredient_list, nutrition = nutrition_info, tags = recipe_tags, url = url)
 
     return (recipe)
 
-# Formats scraped recipes into a dataframe
-
-def get_recipe_df(recipe_urls):
+def get_ingredient_df(recipe_urls, print_recipe_interval):
     
     recipe_data = []
     n_recipes = len(recipe_urls)
 
     for i in range(n_recipes):
-        try:
-            r = get_recipe_info(recipe_urls[i])
-            ingredient_list = [(' '.join([f.value, f.unit, f.food, f.notes])).strip() for f in r.ingredients]
-            recipe_df = pd.DataFrame([r.title, r.nutrition.calories, r.nutrition.protein, r.nutrition.fat, r.nutrition.carbs, ingredient_list, r.tags, r.url]).transpose()
-            recipe_df.columns = ['Title', 'Calories', 'Protein', 'Fat', 'Carbs', 'Ingredients', 'Tags', 'URL']
-            recipe_data.append(recipe_df)
-        except:
-            print(recipe_urls[i])
-            pass
 
-    all_recipes = pd.concat(recipe_data)
-        
-    return(all_recipes)
-
-recipes = []
-
-for i in range(1, 100):
-    try:
-        recipes.append(get_recipe_urls(i))
-        # print('Page %d complete' % i)
-    except:
-        print('No more recipes found.')
-        break
-
-recipes = [url for list in recipes for url in list]
-
-print('\n%d recipes found' % len(recipes))
-
-j = 0
-
-for i in range(1, 18):
-
-    print(j, i*100)
-    
-    recs = recipes[j:i*100]
-    df = get_recipe_df(recs)
-    df.to_csv('mb_output_test' + str(i) + '.csv', index = False)
-    j += 100
-
-df_list = []
-
-for i in range(1, 18):
-    
-    df = pd.read_csv('mb_output_test' + str(i) + '.csv')
-    df_list.append(df)
-
-df = pd.concat(df_list)
-    
-df = df[(df['Ingredients'] != '[]') & (df['Tags'] != '[]')]
-
-df.to_csv('output_file.csv', index = False)
-
-# Formats scraped recipes into a dataframe
-
-def get_ingredient_df(recipe_urls):
-    
-    recipe_data = []
-    n_recipes = len(recipe_urls)
-
-    for i in range(n_recipes):
         r = get_recipe_info(recipe_urls[i])
-        ingredient_values = [f.value for f in r.ingredients]
+
+        ingredient_amts = [f.value for f in r.ingredients]
         ingredient_units  = [f.unit for f in r.ingredients]
-        ingredient_foods  = [f.food for f in r.ingredients]
+        ingredient_items  = [f.food for f in r.ingredients]
         ingredient_notes  = [f.notes for f in r.ingredients]
-        recipe_df = pd.DataFrame([r.title, ingredient_values, ingredient_units, ingredient_foods, ingredient_notes, r.tags, r.url]).transpose()
-        recipe_df.columns = ['Title', 'IValues', 'IUnits', 'IFoods', 'INotes', 'Tags', 'URL']
-        recipe_df = recipe_df.explode(column = ['IValues', 'IUnits', 'IFoods', 'INotes'])
+
+        recipe_df = pd.DataFrame([r.title, 
+                                  ingredient_amts, ingredient_units, ingredient_items, ingredient_notes,
+                                  r.nutrition.calories, r.nutrition.protein, r.nutrition.fat, r.nutrition.carbs,
+                                  r.tags, 
+                                  r.url]).transpose()
+        
+        recipe_df.columns = ['recipeTitle', 
+                             'ingredAmount', 'ingredUnit', 'ingredItem', 'ingredNotes', 
+                             'recipeCalories', 'recipeProtein', 'recipeFat', 'recipeCarbs', 
+                             'recipeTags', 
+                             'recipeURL']
+        
+        recipe_df = recipe_df.explode(column = ['ingredAmount', 'ingredUnit', 'ingredItem', 'ingredNotes'])
+
         recipe_data.append(recipe_df)
 
+        if print_recipe_interval != 0 and i % print_recipe_interval == 0:
+            print('Recipes scanned so far: %d' % i)
+
     all_recipes = pd.concat(recipe_data)
-        
+
     return(all_recipes)
 
-j = 0
+def scrape_mb_recipes(max_pages, print_page_interval, print_recipe_interval):
 
-for i in range(1, 18):
+    start = time.time()
 
-    print(j, i*100)
+    recipe_urls = []
+
+    for i in range(1, max_pages):
+        try:
+            recipe_urls.append(get_recipe_urls(i))
+
+            if print_page_interval != 0 and i % print_page_interval == 0:
+                print('Page %d complete' % i)
+
+        except:
+            print('No more recipes found.')
+            print('Total pages: %d' % (i - 1))
+            break
+
+    recipe_urls = [url for list in recipe_urls for url in list]
+    n_recipes = len(recipe_urls)
+
+    print('\n%d potential recipes URLs found\n' % n_recipes)
+
+    final_df = get_ingredient_df(recipe_urls, print_recipe_interval)
+
+    rejected_recipes = set(list(final_df[final_df['ingredItem'].isna()].copy()['recipeURL']))
     
-    recs = recipes[j:i*100]
-    df = get_ingredient_df(recs)
-    df.to_csv('mb_ingredient_output_test' + str(i) + '.csv', index = False)
-    print(df.shape)
-    j += 100
+    final_df = final_df[final_df['ingredItem'].notna()]
+    accepted_recipes = len(set(list(final_df['recipeURL'])))
 
-df_list = []
+    print('\nTotal accepted recipes: %d' % accepted_recipes)
+    print('Total rejected recipes: %d' % len(rejected_recipes))
+    print('\nThe following recipe URLs did not contain a recipe: ')
 
-for i in range(1, 18):
-    
-    df = pd.read_csv('mb_ingredient_output_test' + str(i) + '.csv')
-    df_list.append(df)
+    for r in rejected_recipes:
+        print(r)
 
-df = pd.concat(df_list)
+    pass_time(start)
 
-df.to_csv('ingredient_output_file.csv', index = False)
+    final_df = final_df[(final_df['Ingredients'] != '[]') &
+                        (final_df['Tags'] != '[]')]
+
+    return(final_df)
